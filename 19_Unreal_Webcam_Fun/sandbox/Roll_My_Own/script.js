acafc02093f4b64d49e7d01b0f0d9e8b0e644c77
Ryan Cahela <ryancahela@gmail.com>
2021-01-15 11:54:19 -0800

    select box populates with available webcams

--- 19_Unreal_Webcam_Fun/sandbox/Roll_My_Own/script.js
+++ 19_Unreal_Webcam_Fun/sandbox/Roll_My_Own/script.js
@@ -1,4 +1,5 @@
 const video = document.querySelector("video");
+const cameraSelect = document.querySelector("#cameraSelect");
 
 const constraints = {
   audio: false,
@@ -6,6 +7,8 @@ const constraints = {
     width: 640,
     height: 480,
     facingMode: "user",
+    deviceId:
+      "40824012d1094e03d97dafa0acb3c4132d4da26fe62f8dd37b4b228d8c5eceb1",
   },
 };
 
@@ -21,3 +24,41 @@ navigator.mediaDevices
   .catch((err) => console.error(err));
 
 video.addEventListener("canplay", () => video.play());
+
+function populateCameraSelect() {
+  navigator.mediaDevices
+    .enumerateDevices()
+    .then(collectVideoInputs)
+    .then((mediaDeviceInfoArray) => {
+      const cameraSelect = document.querySelector("#cameraSelect");
+      const optionElements = mediaDeviceInfoArray
+        .map((mediaDeviceInfo) => {
+          console.log("mediaDevicInfo", mediaDeviceInfo);
+          return `
+          <option data-deviceId="${mediaDeviceInfo.deviceId}">${mediaDeviceInfo.label}</option> 
+        `;
+        })
+        .join("");
+      cameraSelect.innerHTML = optionElements;
+    })
+    .catch((err) => console.error(err));
+}
+
+function collectVideoInputs(devices) {
+  return devices.filter((device) => device.kind === "videoinput");
+}
+
+function insertDeviceInfoIntoDom(mediaDeviceInfoArray, parentElement) {
+  const parent = document.querySelector(parentElement);
+  const optionElements = mediaDeviceInfoArray
+    .map((mediaDeviceInfo) => {
+      console.log("mediaDevicInfo", mediaDeviceInfo);
+      return `
+      <option data-deviceId="${mediaDeviceInfo.deviceId}">${mediaDeviceInfo.label}</option> 
+    `;
+    })
+    .join("");
+  parent.innerHTML = optionElements;
+}
+
+populateCameraSelect();
