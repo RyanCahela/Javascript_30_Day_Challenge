46d155c8b72765cb539aaa9b2e47c86ec29fc271
Ryan Cahela <ryancahela@gmail.com>
2021-01-15 12:07:35 -0800

    camera select default to first option on page load

--- 19_Unreal_Webcam_Fun/sandbox/Roll_My_Own/script.js
+++ 19_Unreal_Webcam_Fun/sandbox/Roll_My_Own/script.js
@@ -1,6 +1,6 @@
 const video = document.querySelector("video");
 const cameraSelect = document.querySelector("#cameraSelect");
-
+populateCameraSelect();
 const constraints = {
   audio: false,
   video: {
@@ -11,17 +11,7 @@ const constraints = {
       "40824012d1094e03d97dafa0acb3c4132d4da26fe62f8dd37b4b228d8c5eceb1",
   },
 };
-
-navigator.mediaDevices
-  .getUserMedia(constraints)
-  .then((mediaStreamObj) => {
-    if ("srcObject" in video) {
-      video.srcObject = mediaStreamObj;
-    } else {
-      video.src = window.URL.createObjectURL(mediaStreamObj);
-    }
-  })
-  .catch((err) => console.error(err));
+getDeviceStream(constraints);
 
 video.addEventListener("canplay", () => video.play());
 
@@ -32,10 +22,12 @@ function populateCameraSelect() {
     .then((mediaDeviceInfoArray) => {
       const cameraSelect = document.querySelector("#cameraSelect");
       const optionElements = mediaDeviceInfoArray
-        .map((mediaDeviceInfo) => {
+        .map((mediaDeviceInfo, index) => {
           console.log("mediaDevicInfo", mediaDeviceInfo);
+          //select first option on page load
+          const selected = index === 0 ? "selected" : "";
           return `
-          <option data-deviceId="${mediaDeviceInfo.deviceId}">${mediaDeviceInfo.label}</option> 
+          <option ${selected} data-device-id="${mediaDeviceInfo.deviceId}">${mediaDeviceInfo.label}</option> 
         `;
         })
         .join("");
@@ -48,17 +40,15 @@ function collectVideoInputs(devices) {
   return devices.filter((device) => device.kind === "videoinput");
 }
 
-function insertDeviceInfoIntoDom(mediaDeviceInfoArray, parentElement) {
-  const parent = document.querySelector(parentElement);
-  const optionElements = mediaDeviceInfoArray
-    .map((mediaDeviceInfo) => {
-      console.log("mediaDevicInfo", mediaDeviceInfo);
-      return `
-      <option data-deviceId="${mediaDeviceInfo.deviceId}">${mediaDeviceInfo.label}</option> 
-    `;
+function getDeviceStream(constraints) {
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then((mediaStreamObj) => {
+      if ("srcObject" in video) {
+        video.srcObject = mediaStreamObj;
+      } else {
+        video.src = window.URL.createObjectURL(mediaStreamObj);
+      }
     })
-    .join("");
-  parent.innerHTML = optionElements;
+    .catch((err) => console.error(err));
 }
-
-populateCameraSelect();
